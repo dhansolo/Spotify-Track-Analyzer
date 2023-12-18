7f8b57bb4e1cf68db60faeebf7068502878139e0
David Han <dhansolo@uw.edu>
2023-12-18 00:47:22 -0800

    Able to authenticate and get a user's top 50 tracks and analyze

---
+++ Spotify Tracker Analyzer/track_analyzer.js
@@ -0,0 +1,108 @@
+const config = require("./config/config")
+const express = require("express");
+const axios = require("axios");
+const querystring = require("querystring");
+const { access } = require("fs");
+
+const app = express();
+const port = 3000;
+
+async function getAccessToken(code) {
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const credentials = `${config.client_id}:${config.client_secret}`;
+    const basicAuthHeader = Buffer.from(credentials).toString('base64');
+
+    const data = {
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: config.redirect_uri,
+        scope: config.scopes // Include the scopes here
+    };
+
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${basicAuthHeader}`
+    };
+
+    const response = await axios.post(tokenUrl, querystring.stringify(data), { headers });
+    return response.data.access_token;
+} 
+
+// Endpoint to initiate Spotify authentication
+app.get('/login', (req, res) => {
+    res.redirect(`https://accounts.spotify.com/authorize?client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&scope=${encodeURIComponent(config.scopes)}&response_type=code&show_dialog=true`);
+  });
+  
+  // Callback endpoint to handle Spotify's redirect after authentication
+  app.get('/callback', async (req, res) => {
+    const code = req.query.code;
+    const accessToken = await getAccessToken(code);
+    track_analyzer(accessToken);
+
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log("\n");
+  });
+
+async function track_analyzer(accessToken) {
+    let track_details = [];
+    try {
+        console.log("Getting your top 50 tracks")
+        let url = config.url + "me/top/tracks?limit=50"
+        let headers = {
+            "Authorization": `Bearer ${accessToken}`
+        }
+        let tracks = await axios.get(url, { headers })
+        for(let track of tracks.data.items) {
+            // console.log(`${track.name} - ${track.artists[0].name} - ${track.album.name}`)
+            track_details.push({
+                "title": track.name,
+                "artist": track.artists[0].name,
+                "album": track.album.name,
+                "id": track.id
+            })
+        }
+        // console.log(track_ids);
+    } catch(err) {
+        console.log(err);
+    }
+
+    let danceability_count = 0;
+    let energy_count = 0;
+    let positivity_count = 0;
+    try {
+        console.log("Analyzing your top 50 tracks");
+        for(let track of track_details) {
+            let url = `${config.url}audio-features/${track.id}`
+            let headers = {
+                "Authorization": `Bearer ${accessToken}`
+            }
+            try {
+                let audio_features = await axios.get(url, { headers })
+                // console.log(`${track.title} - ${track.artist} - ${track.album}`)
+                // console.log("Danceability: " + audio_features.data.danceability);
+                // console.log("Energy: " + audio_features.data.energy);
+                // console.log("Positivity: " + audio_features.data.valence)
+                danceability_count += audio_features.data.danceability
+                energy_count += audio_features.data.energy
+                positivity_count += audio_features.data.valence
+            } catch(err) {
+                console.log(err);
+            }
+        }
+    } catch(err) {
+        console.log(err);
+    }
+
+    let avg_danceability = (danceability_count/50);
+    let avg_energy = (energy_count/50);
+    let avg_positivity = (positivity_count/50);
+    console.log("\n");
+    console.log("Your results");
+    console.log("Danceability: " + avg_danceability);
+    console.log("Energy: " + avg_energy);
+    console.log("Positivity: " + avg_positivity)
+
+}
