16d1e0342b385e922202c1598b0e69a19d89f4a5
David Han <dhansolo@uw.edu>
2023-12-20 21:42:38 -0800

    Building recommendations off of results and creating a playlist with recommended tracks

--- Spotify Tracker Analyzer/track_analyzer.js
+++ Spotify Tracker Analyzer/track_analyzer.js
@@ -31,23 +31,32 @@ async function getAccessToken(code) {
 // Endpoint to initiate Spotify authentication
 app.get('/login', (req, res) => {
     res.redirect(`https://accounts.spotify.com/authorize?client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&scope=${encodeURIComponent(config.scopes)}&response_type=code&show_dialog=true`);
-  });
+});
   
-  // Callback endpoint to handle Spotify's redirect after authentication
-  app.get('/callback', async (req, res) => {
+// Callback endpoint to handle Spotify's redirect after authentication
+app.get('/callback', async (req, res) => {
     const code = req.query.code;
     const accessToken = await getAccessToken(code);
     track_analyzer(accessToken);
+});
 
-  });
-  
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
     console.log("\n");
-  });
+});
+
+async function get_user(accessToken) {
+    let url = config.url + "me";
+    let headers = {
+        "Authorization": `Bearer ${accessToken}`
+    }
+    return await axios.get(url, { headers });
+}
 
 async function track_analyzer(accessToken) {
     let track_details = [];
+    let seed_tracks = [];
+    let seed_artists = [];
     try {
         console.log("Getting your top 50 tracks")
         let url = config.url + "me/top/tracks?limit=50"
@@ -55,26 +64,33 @@ async function track_analyzer(accessToken) {
             "Authorization": `Bearer ${accessToken}`
         }
         let tracks = await axios.get(url, { headers })
+
         for(let track of tracks.data.items) {
             // console.log(`${track.name} - ${track.artists[0].name} - ${track.album.name}`)
             track_details.push({
                 "title": track.name,
                 "artist": track.artists[0].name,
+                "artistId": track.artists[0].id,
                 "album": track.album.name,
                 "id": track.id
             })
         }
-        // console.log(track_ids);
+        for(let track of track_details) {
+            if(seed_tracks.length < 5) {
+                seed_tracks.push(track.id);
+            }
+            if(seed_artists.length < 5) {
+                seed_artists.push(track.artistId)
+            }
+        }
     } catch(err) {
-        console.log(err);
+        console.log(err.response.data);
     }
 
     let danceability_count = 0;
     let energy_count = 0;
     let positivity_count = 0;
     let duration_count = 0;
-    let artist_count = {};
-    let genre_count = {};
     try {
         console.log("Analyzing your top 50 tracks");
         for(let track of track_details) {
@@ -92,22 +108,12 @@ async function track_analyzer(accessToken) {
                 energy_count += audio_features.data.energy;
                 positivity_count += audio_features.data.valence;
                 duration_count += audio_features.data.duration_ms;
-
-                if(track.artist in artist_count) {
-                    artist_count[track.artist] = artist_count[track.artist] + 1;
-                } else {
-                    artist_count[track.artist] = 1;
-                }
             } catch(err) {
-                console.log(err);
+                console.log(err.response.data);
             }
         }
-        let artist_values = Object.entries(artist_count);
-        artist_values.sort((a, b) => b[1] - a[1]);
-        let top_five = artist_values.slice(0, 5)
-        artist_count = Object.fromEntries(top_five);
     } catch(err) {
-        console.log(err);
+        console.log(err.response.data);
     }
 
     let avg_danceability = (danceability_count/50);
@@ -119,7 +125,39 @@ async function track_analyzer(accessToken) {
     console.log("Energy: " + avg_energy);
     console.log("Positivity: " + avg_positivity)
     console.log("\n");
-    console.log("Top Artists")
-    console.log(Object.keys(artist_count).toString());
+
+    try {
+        console.log("Getting Recommendations");
+        let url = config.url + `recommendations?seed_tracks=${seed_tracks}&target_daceability=${avg_danceability}&target_energy=${avg_energy}&target_valence=${avg_positivity}`
+        let headers = {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${accessToken}`
+        }
+        let recs = await axios.get(url, { headers })
+
+        let track_uris = [];
+        for(let rec of recs.data.tracks) {
+            track_uris.push(rec.uri);
+        }
+        console.log("Creating Playlist")
+        let user = await get_user(accessToken)
+        let playlist_url = config.url + `users/${user.data.id}/playlists`;
+        let data = {
+            "name": "Your Recommendations",
+            "description": "Playlist created by Track Analyzer",
+            "public": false
+        }
+        let new_playlist = await axios.post(playlist_url, data, { headers });
+        let playlist_update_url = config.url + `playlists/${new_playlist.data.id}/tracks`
+        let tracks_for_playlist = {
+            'uris': track_uris,
+            "position": 0
+        }
+        await axios.post(playlist_update_url, tracks_for_playlist, { headers })
+        console.log(`Your recommended songs have been added to a new playlist: ${new_playlist.data.external_urls.spotify}`)
+        track_uris = [];
+    } catch(err) {
+        console.log(err.response.data);
+    }
 
 }
